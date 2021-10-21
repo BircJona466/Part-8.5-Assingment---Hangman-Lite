69db49178c58d41465640c92cb9b954c91b93147
Jonah Birch <BircJona466@GoTVDSB.ca>
2021-10-21 12:39:31 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_8._5_Assingment___Hangman_Lite
+{
+    public partial class HangManLite : Form
+    {
+        public HangManLite()
+        {
+            InitializeComponent();
+        }
+    }
+}
