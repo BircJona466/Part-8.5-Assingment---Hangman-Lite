c4dc348d04800110d1d1467eed6ff396b1ff1a7c
Jonah Birch <BircJona466@GoTVDSB.ca>
2021-11-01 12:43:30 -0400

    done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstUsedLetters = new System.Windows.Forms.ListBox();
+            this.lblGuessedLetters = new System.Windows.Forms.Label();
+            this.lblLivesLeft = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblSecretWord = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lstUsedLetters
+            // 
+            this.lstUsedLetters.FormattingEnabled = true;
+            this.lstUsedLetters.Location = new System.Drawing.Point(12, 274);
+            this.lstUsedLetters.Name = "lstUsedLetters";
+            this.lstUsedLetters.Size = new System.Drawing.Size(113, 147);
+            this.lstUsedLetters.TabIndex = 0;
+            // 
+            // lblGuessedLetters
+            // 
+            this.lblGuessedLetters.AutoSize = true;
+            this.lblGuessedLetters.BackColor = System.Drawing.Color.Transparent;
+            this.lblGuessedLetters.Font = new System.Drawing.Font("Wide Latin", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessedLetters.Location = new System.Drawing.Point(-3, 257);
+            this.lblGuessedLetters.Name = "lblGuessedLetters";
+            this.lblGuessedLetters.Size = new System.Drawing.Size(158, 14);
+            this.lblGuessedLetters.TabIndex = 1;
+            this.lblGuessedLetters.Text = "Guessed Letters";
+            // 
+            // lblLivesLeft
+            // 
+            this.lblLivesLeft.AutoSize = true;
+            this.lblLivesLeft.BackColor = System.Drawing.Color.Transparent;
+            this.lblLivesLeft.Font = new System.Drawing.Font("Wide Latin", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLivesLeft.Location = new System.Drawing.Point(118, 98);
+            this.lblLivesLeft.Name = "lblLivesLeft";
+            this.lblLivesLeft.Size = new System.Drawing.Size(115, 14);
+            this.lblLivesLeft.TabIndex = 3;
+            this.lblLivesLeft.Text = "Lives Left : ";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(144, 146);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(40, 20);
+            this.txtGuess.TabIndex = 4;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(196, 146);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(48, 20);
+            this.btnGuess.TabIndex = 5;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.BackColor = System.Drawing.Color.Transparent;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(60, 41);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(0, 39);
+            this.lblWord.TabIndex = 6;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.Font = new System.Drawing.Font("MV Boli", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(132, 115);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(125, 82);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Visible = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblSecretWord
+            // 
+            this.lblSecretWord.AutoSize = true;
+            this.lblSecretWord.BackColor = System.Drawing.Color.Transparent;
+            this.lblSecretWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecretWord.Location = new System.Drawing.Point(111, 43);
+            this.lblSecretWord.Name = "lblSecretWord";
+            this.lblSecretWord.Size = new System.Drawing.Size(232, 55);
+            this.lblSecretWord.TabIndex = 8;
+            this.lblSecretWord.Text = "________";
             // 
             // HangManLite
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::Part_8._5_Assingment___Hangman_Lite.Properties.Resources.Hangman0_copy;
             this.ClientSize = new System.Drawing.Size(404, 480);
+            this.Controls.Add(this.lblSecretWord);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblLivesLeft);
+            this.Controls.Add(this.lblGuessedLetters);
+            this.Controls.Add(this.lstUsedLetters);
             this.Name = "HangManLite";
             this.Text = "HangMan Lite";
+            this.Load += new System.EventHandler(this.HangManLite_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstUsedLetters;
+        private System.Windows.Forms.Label lblGuessedLetters;
+        private System.Windows.Forms.Label lblLivesLeft;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblSecretWord;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,147 @@ namespace Part_8._5_Assingment___Hangman_Lite
 {
     public partial class HangManLite : Form
     {
+        Random generator = new Random();
+        List<string> words = new List<string>();
+       
+
+        string word;
+        string displayWord;
+        int lives = 6;
+        int wordlength;
+        
+        List<string> guesses = new List<string>();
+       
         public HangManLite()
         {
             InitializeComponent();
         }
-    }
+
+        
+
+        private void HangManLite_Load(object sender, EventArgs e)
+        {
+            lblLivesLeft.Text = "Lives Left : 6";
+            lstUsedLetters.DataSource = guesses;
+            lblSecretWord.Text = displayWord;
+            words.Add("JONAH");
+            words.Add("BIRCH");
+            words.Add("CODING");
+            words.Add("JOKER");
+            words.Add("AZURE");
+            words.Add("APEX");
+            int randWord = generator.Next(words.Count);
+            word = (words[randWord]);
+            wordlength = word.Length;
+            string underscore = new String('_', wordlength);
+            displayWord = underscore;
+            lblSecretWord.Text = underscore;
+
+
+        }
+
+
+        private void RefreshNumberListbox()
+        {
+            lstUsedLetters.DataSource = null;
+            lstUsedLetters.DataSource = guesses;
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+
+            txtGuess.MaxLength = 1;
+            txtGuess.Text = txtGuess.Text.ToUpper();
+            int index = word.IndexOf(txtGuess.Text);
+                
+
+            
+            if (word.IndexOf(txtGuess.Text) != -1)
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    displayWord = displayWord.Remove(index, 1);
+                    lblSecretWord.Text = displayWord;
+                    displayWord = displayWord.Insert(index, txtGuess.Text);
+                    lblSecretWord.Text = displayWord;
+
+                    
+                }
+                
+
+               
+
+               
+
+            }
+            else if (guesses.Contains(txtGuess.Text))
+            {
+
+            }
+            else if (displayWord == word)
+            {
+                lblLivesLeft.Text = "You Win!";
+                btnExit.Visible = true;
+
+                lstUsedLetters.Visible = false;
+                
+                lblGuessedLetters.Visible = false;
+            }
+
+            
+            else
+            {
+                guesses.Add(txtGuess.Text);
+                RefreshNumberListbox();
+                lives = lives - 1;
+                lblLivesLeft.Text = ("Lives Left : " + lives + ".");
+            }
+
+
+            
+            
+                
+            if (lives == 5)
+            {
+                    this.BackgroundImage = Properties.Resources.Hangman1_copy;
+            }
+            else if (lives == 6)
+            {
+
+            }
+            else if (lives == 4)
+            {
+                    this.BackgroundImage = Properties.Resources.Hangman2_copy;
+
+            }
+            else if (lives == 3)
+            {
+                    this.BackgroundImage = Properties.Resources.Hangman3_copy;
+            }
+            else if (lives == 2)
+            {
+                    this.BackgroundImage = Properties.Resources.Hangman4_copy;
+            }
+            else if (lives == 1)
+            {
+                    this.BackgroundImage = Properties.Resources.Hangman5_copy;
+            }
+            else
+            {
+                    lblLivesLeft.Text = "You Lose!";
+                    btnExit.Visible = true;
+                    
+                    lstUsedLetters.Visible = false;
+                    this.BackgroundImage = Properties.Resources.Hangman6_copy;
+                    lblGuessedLetters.Visible = false;
+            }
+
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }         
 }
